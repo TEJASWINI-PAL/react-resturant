cf8d12b62439ddc3a0b18d46865955af27778525
TEJASWINI-PAL <paltejaswani199@gmail.com>
2022-07-18 09:39:02 +0530

    files added

---
+++ src/components/Basics/menuApi.js
@@ -0,0 +1,141 @@
+const Menu =[
+    {
+        id:1,
+        image:"images/maggie.jpg" ,
+        name:"maggie",
+        category:"evening",
+        price:20,
+        Description:"lorem",
+    },
+     
+    {
+        id: 2,
+        image: "images/chola.jpg" ,
+        name: "chola",
+        category: "lunch",
+        price: 30,
+        Description: "lorem",
+    },
+
+    {
+        id: 3,
+        image: "images/vegthali.jpg" ,
+        name: "vegthali",
+        category: "lunch",
+        price: 40,
+        Description: "lorem",
+    },
+    {
+        id: 4,
+        image: "images/nonvegthal.jpg" ,
+        name: "nonvegthali",
+        category: "dinner",
+        price: 30,
+        Description:"lorem",
+    },
+
+    {
+        id: 5,
+        image: "images/dosa.jpg" ,
+        name: "dosa",
+        category: "breakfast",
+        price: 30,
+        Description: "lorem",
+    },
+    
+    {
+        id: 6,
+        image: "images/pizza.jpg" ,
+        name: "pizza",
+        category: "evening",
+        price: 30,
+        Description: "lorem",
+    },
+
+    {
+        id: 7,
+        image: "images/momos.jpg" ,
+        name: "momos",
+        category: "evening",
+        price: 40,
+        Description: "lorem",
+    },
+
+    {
+        id: 8,
+        image: "images/pakode.jpg" ,
+        name: "pakode",
+        category: "breakfast",
+        price: 25,
+        Description: "lorem",
+    },
+
+    {
+        id: 9,
+        image: "images/pavbhaji.jpg" ,
+        name: "pavbhaji",
+        category: "evening",
+        price: 50,
+        Description: "lorem",
+    },
+
+    {
+        id:10,
+        image: "images/pasta.jpg" ,
+        name: "pasta",
+        category: "breakfast",
+        price: 30,
+        Description: "lorem",
+    },
+
+    {
+        id: 11,
+        image: "images/samosa.jpg" ,
+        name: "samosa",
+        category: "breakfast",
+        price: 30,
+        Description: "lorem",
+    },
+    {
+        id: 12 ,
+        image: "images/corn.jpg" ,
+        name: "corn",
+        category: "breakfast",
+        price: 30,
+        Description: "lorem",
+    },
+
+    {
+        id: 13,
+        image: "images/rajmarice.jpg" ,
+        name: "rajmarice",
+        category: "dinner",
+        price: 30,
+        Description: "lorem",
+    },
+
+    {
+
+        id: 14,
+        image: "images/sweet.jpg" ,
+        name: "sweet",
+        category: "dinner",
+        price: 30,
+        Description: 
+        "lorem"
+        
+      
+    
+    },
+]
+
+export default Menu;
+
+    
+
+
+
+    
+
+    
+
